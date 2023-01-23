4068547581f6a262f6df90d1835f50ff7ea43fb5
Facundo Meniy <facundomeniy@gmail.com>
2023-01-23 15:25:00 -0300

    finished animations

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const appearOptions = {
     threshold: 1,
 };
 
-
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
     entries.forEach(entry => {
         if(!entry.isIntersecting){
@@ -18,3 +17,31 @@ const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 })
+
+const about = document.getElementById('about-nav');
+const aboutDiv = document.getElementById('about');
+
+const tools = document.getElementById('tools-nav');
+const toolsDiv = document.getElementById('tools');
+
+const work = document.getElementById('work-nav');
+const workDiv = document.getElementById('work');
+
+const contact = document.getElementById('contact-nav');
+const contactDiv = document.getElementById('contact');
+
+about.addEventListener('click', () => {
+    aboutDiv.scrollIntoView({ behavior: 'smooth' });
+});
+
+tools.addEventListener('click', () => {
+    toolsDiv.scrollIntoView({ behavior: 'smooth' });
+});
+
+work.addEventListener('click', () => {
+    workDiv.scrollIntoView({ behavior: 'smooth' });
+});
+
+contact.addEventListener('click', () => {
+    contactDiv.scrollIntoView({ behavior: 'smooth' });
+});
